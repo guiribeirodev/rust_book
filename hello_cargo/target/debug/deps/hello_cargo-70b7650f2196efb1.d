04d2b3f98a8ce024fec45f0a00194230a26c4631
guiribeirodev <guiribeirodev@gmail.com>
2024-09-08 12:16:55 -0300

    Hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1.d
@@ -0,0 +1,5 @@
+/home/guiribeiro/projects/rust/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1: src/main.rs
+
+/home/guiribeiro/projects/rust/hello_cargo/target/debug/deps/hello_cargo-70b7650f2196efb1.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/guiribeiro/projects/rust/hello_cargo/target/debug/hello_cargo: /home/guiribeiro/projects/rust/hello_cargo/src/main.rs
